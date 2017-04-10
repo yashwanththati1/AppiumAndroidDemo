2c7b506caaf4331a19b49ffe25f975f6ad28401b
yashwanththati1 <thati.k@spotqa.com>
2017-04-10 18:06:58 +0530

    Updated AppiumAndroidDemo.Java

--- src/test/java/com/spotqa/mobile/android/AppiumAndroidDemo.java
+++ src/test/java/com/spotqa/mobile/android/AppiumAndroidDemo.java
@@ -6,6 +6,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by SpotQA on 10-04-2017.
@@ -23,9 +24,21 @@ public class AppiumAndroidDemo {
         capabilities.setCapability("app", System.getProperty("user.dir")+"\\resources\\paytm.apk");
         capabilities.setCapability("appPackage", "net.one97.paytm");
         capabilities.setCapability("appActivity", "net.one97.paytm.AJRJarvisSplash");
+        capabilities.setCapability("autoAcceptAlerts", true);
 
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        driver.findElementById("net.one97.paytm:id/btn_language_continue_button").click();
 
-        System.out.println(driver.getTitle());
+        if(driver.findElementsById("com.android.packageinstaller:id/dialog_container").size()>0){
+            driver.findElementById("com.android.packageinstaller:id/dialog_container").findElementById("com.android.packageinstaller:id/permission_allow_button").click();
+        }
+
+        if(driver.findElementsById("com.android.packageinstaller:id/dialog_container").size()>0){
+            driver.findElementById("com.android.packageinstaller:id/dialog_container").findElementById("com.android.packageinstaller:id/permission_allow_button").click();
+        }
+
+        ((AndroidDriver)driver).findElementByAndroidUIAutomator("new UiSelector().text(\"Do It Later\")").click();
+        ((AndroidDriver)driver).findElementByAndroidUIAutomator("new UiSelector().resourceId(\"net.one97.paytm:id/help_overlay\")").click();
     }
 }

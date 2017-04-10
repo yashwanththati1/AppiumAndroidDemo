5ec6e265c42e5bff89784c3e0863b0a7cf1a633a
yashwanththati1 <thati.k@spotqa.com>
2017-04-10 15:15:00 +0530

    First Commit

---
+++ src/test/java/com/spotqa/mobile/android/AppiumAndroidDemo.java
@@ -0,0 +1,29 @@
+package com.spotqa.mobile.android;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by SpotQA on 10-04-2017.
+ */
+public class AppiumAndroidDemo {
+
+    static AppiumDriver<MobileElement> driver;
+    static DesiredCapabilities capabilities;
+
+    public static void main(String[] args) throws MalformedURLException{
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platform", "Android");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("deviceName", "192.168.155.101:5555");
+        capabilities.setCapability("app", System.getProperty("user.dir")+"\\resources\\paytm.apk");
+        capabilities.setCapability("appPackage", "net.one97.paytm");
+        capabilities.setCapability("appActivity", "net.one97.paytm.AJRJarvisSplash");
+
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
